25faba1fcebac2992b77b1fd84b8e1108b432b71
kinjal-mitra <kinjalmitra03@gmail.com>
2025-04-10 17:40:06 +0530

    Initial comit with folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medical_Chatbot_GenAI_Project",
+    version = '0.0.0',
+    author = 'Kinjal Mitra',
+    author_email= "kinjalmitra03@gmail.com",
+    packages = find_packages(),
+    install_required = []
+)
